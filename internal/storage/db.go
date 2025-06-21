9e69df6abca6b5a9bfe4d15a3f0a2c4bb0baeb65
Cuga77 <bogatov2021@mail.ru>
2025-06-22 02:21:38 +0300

    add close method

--- internal/storage/db.go
+++ internal/storage/db.go
@@ -14,9 +14,10 @@ type Page struct {
 }
 
 type Storer interface {
-	StorePage(ctx context.Context, page *Page) (*int64, error)
+	StorePage(ctx context.Context, page *Page) (int64, error)
 	GetNextPageToIndex(ctx context.Context) (*Page, error)
 	UpdatePageIndexed(ctx context.Context, pageID int64) error
 	FindDocumentsByTerm(ctx context.Context, term string) ([]int64, error)
 	GetPagesByIDs(ctx context.Context, ids []int64) ([]*Page, error)
+	Close()
 }
